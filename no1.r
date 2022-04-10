3840c0bd21cb97ec5291cf3ca11bb665d6517b9f
danielcristho <pepuhodaniel93.learn@gmail.com>
2022-04-10 21:07:53 +0900

    fix

--- no1.r
+++ no1.r
@@ -1,7 +1,20 @@
 #A
-p = 0.2
+p = 0.20
 n = 3
 dgeom(x = n, prob = p)
+print(dg)
 
 #B
 mean(rgeom(n = 10000, prob = p) == 3)
+
+#C
+
+#D
+hist(rgeom(n,p),main="Histogram of Geometric",xlab="n")
+
+#E
+p = 0.2
+mean = 1/p
+mean
+var = (1-p)/p^2
+var
